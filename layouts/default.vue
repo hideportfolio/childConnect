2f2ddb5d9d7d861865ce0c62336112dc1a0f8a83
rear0146 <shiraishi.chihiro@uec.ac.jp>
2021-09-13 22:07:43 +0900

    共通CSS追加

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+<style>
+    body{
+        background-color: #A1CAE2;
+    }
+    h1{
+        width: 100%;
+        font-size: 24px;
+        color: #FFFFFF;
+        background-color: #A1CAE2;
+        border-radius: 5px;
+        padding: 8px;
+        margin-bottom: 5px;
+    }
+  .card{
+        margin: 20px auto;
+        background-color: #FFFFFF;
+        max-width: 800px;
+        padding: 10px;
+  }
+  .submit{
+    background-color: #FFFFFF;
+    color: #825959;
+    width: 240px;
+    height: 50px;
+    text-align: center;
+    border: 2px #825959 solid;
+    border-radius: 30px;
+  }
+  .submit:hover{
+    background-color: #825959;
+    color: #FFFFFF;
+    transition: 0.3s;
+  }
+</style>
