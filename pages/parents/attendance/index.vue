d509dfa43b922426529ad558cc7c292e88bf6b83
hideportfolio <hidehis111@gmail.com>
2021-09-13 22:53:29 +0900

    [add]attend UI

--- pages/parents/attendance/index.vue
+++ pages/parents/attendance/index.vue
@@ -1,9 +1,136 @@
 <template>
-  <div>
-    <p>This is attendance page.</p>
-    <button @click="postAttendace()">login</button>
+  <div class="a">
+    <div class="card">
+      <h1>出席登録</h1>
+      <div class="date">2021/9/17</div>
+      <p>
+        <input name="attend" type="radio" value="1" id="attend">
+        <label for="attend" class="attend">出席</label>
+
+        <input name="attend" type="radio" value="0" id="absence">
+        <label for="absence" class="absence">欠席</label>
+      </p>
+
+      <div class="submit-box">
+        <button @click="postAttendace()" class="submit">登録</button>
+      </div>
+    </div>
   </div>
+
 </template>
+
+<style>
+.a {
+  width: 100%;
+  height: 800px;
+  background: #A1CAE2;
+}
+.card {
+  padding: 15px;
+  width: 90%;
+  margin: 30px auto;
+}
+h1 {
+  color: #FFFFFF;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 21px;
+
+  background: #A1CAE2;
+  width: 265px;
+  height: 37px;
+  border-radius: 12px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+p {
+  display:flex;
+  justify-content:center;
+}
+
+.date {
+  /* 2021/9/17 */
+  color: #825959;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 21px;
+
+  width: 100px;
+  height: 21px;
+
+  margin: 20px auto;
+}
+
+input[type=radio]{
+  display: none;
+}
+
+.attend {
+  color: #6CED9C;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+
+  background-color: white;
+  width: 116px;
+  height: 81px;
+
+  border: 3px solid #6CED9C;
+  box-sizing: border-box;
+  border-radius: 10px;
+  margin: 10px;
+  line-height: 81px;
+
+  text-align: center;
+}
+
+.absence {
+  color: #FF7676;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 81px;
+
+  background-color: white;
+  width: 112px;
+  height: 82px;
+
+  border: 3px solid #FF7676;
+  box-sizing: border-box;
+  border-radius: 10px;
+
+  text-align: center;
+  margin: 10px;
+}
+
+input[type=radio]:checked + label.attend {
+  color: white;
+  background-color: #6CED9C;
+}
+
+input[type=radio]:checked + label.absence {
+  color: white;
+  background-color: #FF7676;
+}
+
+.submit-box {
+  display:flex;
+  justify-content:center;
+}
+
+.submit {
+  margin: 20px auto;
+}
+
+</style>
 
 <script>
 import Auth from '@aws-amplify/auth'
@@ -11,6 +138,7 @@ import API, { graphqlOperation } from '@aws-amplify/api'
 import { createAttendance } from '~/graphql/mutations'
 
 export default {
+  layout: 'default',
   middleware: 'auth',
   data () {
     return {
@@ -18,14 +146,17 @@ export default {
       attendanceType: [
         'ATTEND',
         'ABUSENT'
-      ]
+      ],
+      date: ''
     }
   },
   methods: {
     async postAttendace () {
       const auth = await Auth.currentUserInfo()
       const now = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000)) // Timezone Tokyo
-      const date = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()
+      // const date = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()
+      this.date = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()
+      const date = this.date
       const res = await API.graphql(graphqlOperation(createAttendance, {
         input: {
           school: 'school',
