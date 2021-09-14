6d80828c0e8bd8d6d414eff197d81109b215dc1a
RyomaYoshida <m01182798e@edu.teu.ac.jp>
2021-09-14 09:54:29 +0900

    ログイン画面追加

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,10 +1,47 @@
 <template>
-  <div>
-    <input v-model="loginid" placeholder="ID">
-    <input v-model="loginpw" placeholder="Password">
-    <button @click="login()">login</button>
+  <div class = "form">
+    <h1>ログイン</h1>
+    <form action="post">
+      <div class="container">
+        <p>ID</p>
+        <input class="ID" v-model="loginid" placeholder="">
+      </div>
+      <div class="container">
+        <p>Password</p>
+        <input class="pass" v-model="loginpw" placeholder="">
+      </div>
+      <br>
+      <br>
+      <div class="submit"><button @click="login()">ログイン</button></div>
+    </form>
   </div>
 </template>
+
+<style>
+  template,h1,p,form,input{margin:0}
+  form{    
+    margin:0 auto;
+    text-align: center;    
+  }
+  .form p{    
+    text-align:left;
+    width:250px;
+    font-size: 20px;
+  }
+  .form-title{
+    text-align: center;
+  }
+  .container {
+    width:250px;
+    margin:0 auto;
+  }
+  .ID, .pass{
+    width:250px;
+  }
+  .submit{
+    text-align: center;
+  }
+</style>
 
 <script>
 import { Auth } from 'aws-amplify'
